df29fdd80990d5e12ab3f52483a5e8c6862daaf7
Amar H <hajrovica@gmail.com>
2011-08-22 03:45:22 +0200

    half way through 0304 video

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'spork', '~> 0.9.0.rc'
+
+group :development, :test do
+  gem 'rspec-rails', '2.0.0.beta.18'
+  gem 'watchr'
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sapp::Application.config.secret_token = '7e2e513716f23b95bd8d2b04f5ddd916913caa88a6d0c7e6ad2e30a360bb6fc828fa36357cec4bf090d610b4da14d9ae153c1dafc4a4036cd78a88d79d7cdd71'
